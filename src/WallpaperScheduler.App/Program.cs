77d44dfd4a9e4b423ab197a2ee420bed7169927c
25neonbyte-cloud <25neonbyte@gmail.com>
2026-09-15 18:31:36 -0300

    fix(dpi): set PerMonitorV2 before WPF startup

---
+++ src/WallpaperScheduler.App/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using FormsApplication = System.Windows.Forms.Application;
+using FormsHighDpiMode = System.Windows.Forms.HighDpiMode;
+
+namespace WallpaperScheduler.App;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        FormsApplication.SetHighDpiMode(FormsHighDpiMode.PerMonitorV2);
+
+        var app = new App();
+        app.InitializeComponent();
+        app.Run();
+    }
+}
