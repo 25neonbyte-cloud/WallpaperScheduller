0813c14440d9e00f7a756060501b80e699a00b42
25neonbyte-cloud <25neonbyte@gmail.com>
2026-09-15 19:38:42 -0300

    test(config): expect schema v3 comfort defaults

--- tests/WallpaperScheduler.Domain.Tests/AppConfigVersionTests.cs
+++ tests/WallpaperScheduler.Domain.Tests/AppConfigVersionTests.cs
@@ -1,4 +1,3 @@
-using Xunit;
 using WallpaperScheduler.Domain;
 
 namespace WallpaperScheduler.Domain.Tests;
@@ -6,9 +5,11 @@ namespace WallpaperScheduler.Domain.Tests;
 public sealed class AppConfigVersionTests
 {
     [Fact]
-    public void DefaultConfig_UsesCoreSchemaV2BeforeVisualComfortModules()
+    public void New_config_uses_schema_v3()
     {
         var config = new AppConfig();
-        Assert.Equal(2, config.Version);
+        Assert.Equal(3, config.Version);
+        Assert.NotNull(config.VisualComfort);
+        Assert.False(config.VisualComfort.Enabled);
     }
 }
